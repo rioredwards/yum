32ad6412e0be9b501f52677ba064c9ebd5294576
Rio Edwards <rioredwards@gmail.com>
2022-12-26 15:05:56 -0800

    Replaced async fs with sync fs

--- replaceInFile.js
+++ replaceInFile.js
@@ -1,8 +1,8 @@
 /* Imports */
 const fs = require("fs");
 const filePathVars = "./colors/yum-vars-color-theme.json";
+const filePathColors = "./colors/theme-color.json";
 const filePathHex = "./themes/yum-color-theme.json";
-const filePathColors = "./colors/colors.js";
 
 /* State Variables */
 const mode = process.argv[2];
@@ -10,6 +10,7 @@ let fsWait = false;
 let colors = null;
 
 function main() {
+    updateColors();
     if (!mode || mode === "sync") {
         varsToHex();
         hexToVars();
@@ -38,88 +39,57 @@ function watch(readFile, callback) {
             }, 100);
             console.log(`${filename} file Changed`);
             if (readFile === filePathColors) {
-                console.log("colors: ", colors);
-                colors = null;
-                colors = require("./colors/colors.js");
-                console.log("newcolors: ", colors);
+                updateColors();
             }
-            // callback(); // NOTE Uncomment
+            callback();
         }
     });
 }
 
-function createNewColors() {
-    fs.readFile(filePathColors, "utf8", function (err, data) {
-        let colorsFile;
-        if (err) {
-            return console.log(err);
-        } else {
-            colorsFile = data;
-            console.log("colorsFile: ", colorsFile);
-            console.log("colorsFile: ", colorsFile.bgDark1);
-            console.log("type of colorsFile: ", typeof colorsFile);
-        }
-    });
+// Read from theme-color.json and create colors obj
+function updateColors() {
+    const colorsFile = fs.readFileSync(filePathColors, "utf8");
+    colors = JSON.parse(colorsFile);
 }
-createNewColors();
 
 // Replaces variables with hexValues
 function varsToHex() {
     // Read from yum-color-theme-vars.json
+    let varsFile = fs.readFileSync(filePathVars, "utf8");
 
-    fs.readFile(filePathVars, "utf8", function (err, data) {
-        let varsFile;
-        if (err) {
-            return console.log(err);
-        } else {
-            varsFile = data;
-        }
+    // create new string with variables replaced with hex values
+    for (const color in colors) {
+        let regex = new RegExp(color, "g");
+        const newVarsFile = varsFile.replace(regex, colors[color]);
+        varsFile = newVarsFile;
 
-        // create new string with variables replaced with hex values
-        for (const color in colors) {
-            let regex = new RegExp(color, "g");
-            const newVarsFile = varsFile.replace(regex, colors[color]);
-            varsFile = newVarsFile;
+        console.log(
+            `In ${filePathHex}: replaced: ${color} with: ${colors[color]}`
+        );
+    }
 
-            console.log(
-                `In ${filePathHex}: replaced: ${color} with: ${colors[color]}`
-            );
-        }
-
-        // rewrite yum-color-theme.json with new string
-        fs.writeFile(filePathHex, varsFile, "utf8", function (err) {
-            if (err) return console.log(err);
-        });
-    });
+    // rewrite yum-color-theme.json with new string
+    fs.writeFileSync(filePathHex, varsFile, "utf8");
 }
 
 // Replaces hexValues with variables
 function hexToVars() {
     // Read from yum-color-theme.json
-    fs.readFile(filePathHex, "utf8", function (err, data) {
-        let hexFile;
-        if (err) {
-            return console.log(err);
-        } else {
-            hexFile = data;
-        }
+    let hexFile = fs.readFileSync(filePathHex, "utf8");
 
-        // create new string with hex values replaced with variables
-        for (const color in colors) {
-            let regex = new RegExp(colors[color], "g");
-            const newHexFile = hexFile.replace(regex, color);
-            hexFile = newHexFile;
+    // create new string with hex values replaced with variables
+    for (const color in colors) {
+        let regex = new RegExp(colors[color], "g");
+        const newHexFile = hexFile.replace(regex, color);
+        hexFile = newHexFile;
 
-            console.log(
-                `In ${filePathVars}: replaced: ${colors[color]} with: ${colors}`
-            );
-        }
+        console.log(
+            `In ${filePathVars}: replaced: ${colors[color]} with: ${colors}`
+        );
+    }
 
-        // rewrite yum-color-theme-vars.json with new string
-        fs.writeFile(filePathVars, hexFile, "utf8", function (err) {
-            if (err) return console.log(err);
-        });
-    });
+    // rewrite yum-color-theme-vars.json with new string
+    fs.writeFileSync(filePathVars, hexFile, "utf8");
 }
 
 main();
